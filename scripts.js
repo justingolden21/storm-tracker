f436be9b867093155870bb648959846111214c83
Justin Golden <justingolden21@gmail.com>
2018-10-03 03:51:52 -0700

    init

---
+++ scripts.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	//random background
+	// let imgNames = ['empty'];
+	let imgNames = ['tarn','rooftop','vents','visions', 'shot', 'empty'];
+	$('body').css('background-image', 'url("img/' + imgNames[Math.floor(Math.random()*imgNames.length)] + '.jpg")');
+
+	$('.btn-minus').on('click', function() {
+		let input = $(this).parent().parent().children('input[type=number]');
+		input.val(parseInt(input.val() )-1 );
+	});
+	$('.btn-plus').on('click', function() {
+		let input = $(this).parent().parent().children('input[type=number]');
+		input.val(parseInt(input.val() )+1 );
+	});
+
+	//easy solution to dealing with inputs, increment, decrement, and clear buttons
+	$('body').on('click', function() {
+		drawMana();
+		drawStorm();
+	});
+	$('body').on('keyup', function() {
+		drawMana();
+		drawStorm();
+	});
+
+});
+
+function drawMana() {
+	$('#manaDiv').html('');
+	for(let i=0; i<$('#blueInput').val(); i++) {
+		$('#manaDiv').append('<img width="32px" src="img/blue.svg">');
+	}
+	for(let i=0; i<$('#redInput').val(); i++) {
+		$('#manaDiv').append('<img width="32px" src="img/red.svg">');
+	}
+	for(let i=0; i<$('#colorlessInput').val(); i++) {
+		$('#manaDiv').append('<img width="32px" src="img/colorless.svg">');
+	}
+}
+function drawStorm() {
+	$('#stormDiv').html('');
+	for(let i=0; i<$('#stormInput').val(); i++) {
+		$('#stormDiv').append('<img width="32px" src="img/energy.svg">');
+	}
+}
